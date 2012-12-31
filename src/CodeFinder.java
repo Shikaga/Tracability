5924b32ca086ef557577c30ac176610417e4fe28
Jon Paul <shikaga@gmail.com>
2012-12-31 10:31:47 +0000

    Refactor

--- src/CodeFinder.java
+++ src/CodeFinder.java
@@ -1,36 +1,42 @@
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 
 public class CodeFinder {
 	
+	private String[] splitFileByComment(String fileString) {
+		String[] splitFile = fileString.split("/\\*\\*\n");
+		return Arrays.copyOfRange(splitFile, 1, splitFile.length);
+	}
+	
 	public Comment[] getComments(File file) throws IOException {
 		String fileString = fileToString(file);
+		String[] splitFile = splitFileByComment(fileString);
 		
-		String[] splitFile = fileString.split("/\\*\\*\n");
-		int commentCount = splitFile.length;
-		if (commentCount == 1) return new Comment[] {};
+		int numberOfComments = splitFile.length;
+		if (numberOfComments == 0) return new Comment[] {};
 		
-		Comment[] comments = new Comment[commentCount-1];
+		Comment[] commentsToReturns = new Comment[numberOfComments];
 		
-		for (int i=1; i < commentCount; i++) {			
-			int lengthOfComment = splitFile[i].indexOf("\n*/");
-			String comment = splitFile[i].substring(0, lengthOfComment);
-			String restOfCode = splitFile[i].substring(lengthOfComment+4);
-			String code = restOfCode.substring(0, restOfCode.indexOf("\n"));
-			comments[i-1] = new Comment(comment, code);//comment;
+		for (int i=0; i < numberOfComments; i++) {
+			String[] commentAndCode = splitFile[i].split("\n\\*/\n"); 
+			String comment = commentAndCode[0];
+			String restOfCode = commentAndCode[1];
+			String firstLineOfcode = restOfCode.substring(0, restOfCode.indexOf("\n"));
+			commentsToReturns[i] = new Comment(comment, firstLineOfcode);
 		}
 		
-		return comments;
+		return commentsToReturns;
 	}
 
 	public String fileToString(File file) throws IOException {
 		BufferedReader br = new BufferedReader(new FileReader(file));
 		
 		String fileString = "";
+		
 		String lineString;
 		while ((lineString = br.readLine()) != null) {
 			fileString += lineString + "\n"; 
